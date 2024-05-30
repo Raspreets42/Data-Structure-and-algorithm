4706a33e8fb8551065a50d5ee53b5e5727259ec3
click2cloud-raspreet <raspreetsingh.pasrija@click2cloud.net>
2024-05-30 18:12:12 +0530

    css for stack

--- src/app/stack/stack.component.ts
+++ src/app/stack/stack.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+// @ts-ignore
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-stack',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./stack.component.scss']
 })
 export class StackComponent {
+  constructor() {
+  }
 
+  onAutoplay: boolean = false;
+  @ViewChild('pdfTable', {static: false}) pdfTable!: ElementRef;
+
+  ngOnChange(){
+    this.onAutoplay = true;
+  }
+  ngOnDestroy(){
+    this.onAutoplay = false;
+  }
+
+  public downloadAsPDF() {
+    const doc = new jsPDF();
+
+    const specialElementHandlers = {
+      '#editor': function () {
+        return true;
+      }
+    };
+
+    const pdfTable = this.pdfTable.nativeElement;
+
+    doc.fromHTML(pdfTable, 15, 15, {
+      width: 190,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save('tableToPdf.pdf');
+  }
 }
